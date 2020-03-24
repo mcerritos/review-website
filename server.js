c8cbedcb944a505d210da6242c3ac1254058e9eb
Melody <leclecticat@gmail.com>
2020-03-24 14:07:30 -0700

    setting up server

---
+++ server.js
@@ -0,0 +1,30 @@
+// dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 4000;
+
+// database
+
+const db = require('./models');
+
+//routes
+
+const routes = require('./routes');
+
+// middleware
+app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.json());
+
+// routes
+app.use('/', routes.views);
+app.use('/api/v1/', routes.api);
+
+// -------------------------------------- START SERVER -------------------------------------- //
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+
+
+
+
